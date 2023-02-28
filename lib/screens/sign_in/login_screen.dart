47e934a866f246759e798166c73d2c2cb66ed618
hashan8991 <wkdhashanpiyumal@gmail.com>
2023-03-01 01:06:05 +0530

    update login screen

--- lib/screens/sign_in/login_screen.dart
+++ lib/screens/sign_in/login_screen.dart
@@ -1,17 +1,26 @@
 import "package:flutter/material.dart";
-//import 'package:tour_drive_frontend/size_config.dart';
 import 'package:tour_drive_frontend/constants.dart';
 import 'package:tour_drive_frontend/screens/forget_password/forget_password_screen.dart';
 import 'package:tour_drive_frontend/screens/loading/loading_screen.dart';
 import 'package:tour_drive_frontend/screens/sign_up/sign_up_screen.dart';
 import 'package:tour_drive_frontend/widgets/default_button.dart';
 import 'package:tour_drive_frontend/widgets/header.dart';
+//import 'package:tour_drive_frontend/size_config.dart';
 
-class LogInScreen extends StatelessWidget {
-
-  static String routeName = "/sign_in";
+class LogInScreen extends StatefulWidget {
   const LogInScreen({super.key});
-  
+
+  @override
+  State<LogInScreen> createState() => _LogInScreenState();
+}
+
+class _LogInScreenState extends State<LogInScreen> {
+  late final String email,password;
+  final formKey = GlobalKey<FormState>();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  bool isLoading = false;
+
   @override
   Widget build(BuildContext context) {
     
@@ -26,43 +35,95 @@ class LogInScreen extends StatelessWidget {
             scrollDirection: Axis.vertical,
             child: Column(
               children: <Widget>[
-                Header(text: "Login", press: () {
+
+                Header(text: "Sign In", press: () {
                   Navigator.push(context, MaterialPageRoute(builder: (context) => const LoadingScreen()));
                 } ),
+
                 SizedBox(height: screenHeight * 0.02),
                 const Text("Hey, Enter your details to get sign in ",),
                 SizedBox(height: screenHeight * 0.04),
-                TextField(
-                  //controller: emailController,
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                    hintText: 'Enter Email',
-                    suffixIcon: const Icon(Icons.email, color: kPrimaryColor,),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(screenHeight * 0.03),
-                    ),
+
+                Form(
+                  key: formKey,
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        controller: emailController,
+                        onSaved: (name)  {
+                          email = name!;
+                        },
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'Please enter your email';
+                          }
+                          if (!RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$').hasMatch(value)) {
+                            return 'Please enter a valid email';
+                          }
+                          return null;
+                        },
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                          labelText: "Email",
+                          floatingLabelStyle: const TextStyle(color: kPrimaryColor),
+                          contentPadding: EdgeInsets.symmetric(horizontal: screenWidth * 0.05, vertical: screenHeight * 0.028),
+                          hintText: 'Enter your email',
+                          suffixIcon: const Icon(Icons.email, color: kPrimaryColor,),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(screenHeight * 0.03),
+                            borderSide: const BorderSide(color: kPrimaryColor),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(screenHeight * 0.03),
+                            borderSide: const BorderSide(color: kPrimaryColor),
+                          )
+                        ),
+                      ),
+                      SizedBox(height: screenHeight * 0.02),
+                      TextFormField(
+                        controller: passwordController,
+                        onSaved: (value)  {
+                          password = value!;
+                        },
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'Please enter your Password';
+                          }
+                          if ( value.length < 6) {
+                            return 'Please enter more than 6 characters';
+                          }
+                          return null;
+                        },
+                        keyboardType: TextInputType.visiblePassword,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          labelText: "Password",
+                          floatingLabelStyle: const TextStyle(color: kPrimaryColor),
+                          contentPadding: EdgeInsets.symmetric(horizontal: screenWidth * 0.05, vertical: screenHeight * 0.028),
+                          hintText: 'Enter your password',
+                          suffixIcon: const Icon(Icons.lock, color: kPrimaryColor,),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(screenHeight * 0.03),
+                            borderSide: const BorderSide(color: kPrimaryColor),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(screenHeight * 0.03),
+                            borderSide: const BorderSide(color: kPrimaryColor),
+                          )
+                        ),
+                      ),
+                    ],
                   ),
                 ),
+                
                 SizedBox(height: screenHeight * 0.02),
-                TextField(
-                  //controller: passwordController,
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    hintText: 'Enter Password',
-                    suffixIcon: const Icon(Icons.lock, color: kPrimaryColor),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(screenHeight * 0.03),
-                    ),
-                  ),
-                ),
-                SizedBox(height: screenHeight * 0.02),
-                Row(
-                  children: [
-                    const Icon(Icons.warning, color: kPrimaryColor,),
-                    SizedBox(width: screenWidth * 0.02,),
-                    const Text("Email Or Password Is Incorrect", ),
-                  ],
-                ),
+                // Row(
+                //   children: [
+                //     const Icon(Icons.warning, color: kPrimaryColor,),
+                //     SizedBox(width: screenWidth * 0.02,),
+                //     const Text("Email Or Password Is Incorrect", ),
+                //   ],
+                // ),
                 TextButton(
                   onPressed: () {
                     Navigator.push(context, MaterialPageRoute(builder: (context) => const ForgetPasswordScreen()));
@@ -79,9 +140,17 @@ class LogInScreen extends StatelessWidget {
                   ),
                 ),
                 SizedBox(height: screenHeight * 0.02),
-                DefaultButton( text: "Login", press: () {
+                
+                DefaultButton( text: "Login", press:  () {
+                  if (formKey.currentState!.validate()) {
                   
-                },),
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Login successfully'), backgroundColor: kPrimaryColor,),
+                  );
+                }
+                },
+                ),
+
                 SizedBox(height: screenHeight * 0.03),
                 Text("Or Sign In With", style: TextStyle(fontSize: screenHeight*0.02, fontWeight: FontWeight.bold),),  
                 SizedBox(height: screenHeight * 0.03),
@@ -120,13 +189,11 @@ class LogInScreen extends StatelessWidget {
                     const Text("Don't have an account ?"),
                     TextButton(
                       onPressed: () {
-                        Navigator.push(context, MaterialPageRoute(builder: (context) =>  SignUpScreen()));
+                       Navigator.push(context, MaterialPageRoute(builder: (context) =>  const SignUpScreen()));
                       },
-                  
                       style: TextButton.styleFrom(
                         foregroundColor: kPrimaryColor,
                       ),
-                      
                       child: Text('Register Now',style: TextStyle(fontSize: screenHeight * 0.02,color: kPrimaryColor,),
                       ),
                     ),
@@ -140,5 +207,3 @@ class LogInScreen extends StatelessWidget {
     );
   }
 }
-
-
