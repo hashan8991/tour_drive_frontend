d0ff1510db10ce45562a67ba8333a01faecf03dd
Udaya Anushanka <122953791+Anush1997@users.noreply.github.com>
2023-03-04 16:44:05 +0530

    landing layout created

---
+++ lib/screens/landing_page/landing_screen.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:tour_drive_frontend/widgets/default_button.dart';
+
+class LandingScreen extends StatefulWidget {
+  const LandingScreen({super.key});
+
+  @override
+  State<LandingScreen> createState() => _LandingScreenState();
+}
+
+class _LandingScreenState extends State<LandingScreen> {
+  List pages = [
+    // HomePage(),
+    // ExplorePage(),
+    // DrivePage(),
+    // ProfilePage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    final double screenWidth = MediaQuery.of(context).size.width;
+    final double screenHeight = MediaQuery.of(context).size.height;
+
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          centerTitle: false,
+          backgroundColor: Colors.transparent, //title eka center krnne meken
+          title: Image.asset(
+            'assets/images/logoNew.png',
+            fit: BoxFit.fill,
+            height: screenHeight * 0.04,
+            width: screenWidth * 0.6,
+          ),
+        ),
+        body: Container(
+          margin: EdgeInsets.all(screenWidth * 0.04),
+          child: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(height: screenHeight * 0.1),
+                Image.asset('assets/images/drive2.png'),
+                SizedBox(height: screenHeight * 0.02),
+                Image.asset('assets/images/tour2.png'),
+              ],
+            ),
+          ),
+        ),
+        // bottomNavigationBar: BottomNavigationBar(items: items),
+      ),
+    );
+  }
+}
