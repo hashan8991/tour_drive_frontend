0056841ee67284f847db07a18a510b368863c691
hashan8991 <wkdhashanpiyumal@gmail.com>
2023-03-07 22:34:02 +0530

    create favorite icon widget

---
+++ lib/widgets/favorite_icon.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class MyFavoriteIconWidget extends StatefulWidget {
+  const MyFavoriteIconWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<MyFavoriteIconWidget> createState() => _MyFavoriteIconWidgetState();
+}
+
+class _MyFavoriteIconWidgetState extends State<MyFavoriteIconWidget> {
+  bool isSelected = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          isSelected = !isSelected;
+        });
+      },
+      child: 
+        Icon(isSelected ? Icons.favorite: Icons.favorite_border,color: isSelected ? Colors.red: Colors.black,),
+      
+    );
+     
+   
+  }
+}
