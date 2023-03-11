89de1988d5dbc4fe89249d891de32de9fcb52cc5
hashan8991 <wkdhashanpiyumal@gmail.com>
2023-03-11 21:04:10 +0530

    update divider widgets

--- lib/widgets/divider_container.dart
+++ lib/widgets/divider_container.dart
@@ -15,20 +15,24 @@ class DividerContainer extends StatelessWidget {
     final double screenHeight = MediaQuery.of(context).size.height;
     return GestureDetector(
       onTap: press,
-      child: Column(
-        children:  [
-          const Divider(thickness: 1.0),
-          SizedBox(height: screenHeight * 0.02,),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text( text, style: TextStyle(fontSize: screenHeight * 0.02,fontWeight: FontWeight.bold)),
-              SizedBox(width: screenHeight * 0.02,),
-              Icon(Icons.arrow_forward_ios, size: screenHeight * 0.021,),
-            ],
-          ),
-          SizedBox(height: screenHeight * 0.02,),
-        ],
+      child: Card(
+        color: Colors.transparent,
+        elevation: 0,
+        child: Column(
+          children:  [
+            const Divider(thickness: 1.0),
+            SizedBox(height: screenHeight * 0.02,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text( text, style: TextStyle(fontSize: screenHeight * 0.02,fontWeight: FontWeight.bold)),
+                SizedBox(width: screenHeight * 0.02,),
+                Icon(Icons.arrow_forward_ios, size: screenHeight * 0.021,),
+              ],
+            ),
+            SizedBox(height: screenHeight * 0.02,),
+          ],
+        ),
       ),
     );
   }
