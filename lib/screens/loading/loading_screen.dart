dc8d06b63df2dad9e8c1623f232da0e3b5e98f6e
hashan8991 <wkdhashanpiyumal@gmail.com>
2023-03-11 21:06:32 +0530

    chage margin size of screen

--- lib/screens/loading/loading_screen.dart
+++ lib/screens/loading/loading_screen.dart
@@ -17,15 +17,15 @@ class LoadingScreen extends StatelessWidget {
     return SafeArea(
       child: Scaffold(
         body: Container(
-          margin: EdgeInsets.all(screenWidth * 0.05),
+          margin: EdgeInsets.symmetric(horizontal: screenWidth * 0.05),
           child: SingleChildScrollView(
             scrollDirection: Axis.vertical,
             child: Column(
               children: <Widget>[
                 SizedBox(
-                  width: screenWidth,
+                  width: screenWidth * 0.7,
                   height: screenHeight * 0.3,
-                  child: Image.asset('assets/images/logo.png')
+                  child: Image.asset('assets/images/logoPic.png')
                 ),
                 SizedBox(
                   width: screenWidth,
