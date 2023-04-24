1de51a6ae98b8fc3cdb91af1faa1d35139551be7
hashan8991 <wkdhashanpiyumal@gmail.com>
2023-04-24 16:26:52 +0530

    comment tour map screen view

--- lib/screens/tour_screen/Single_tour_screen/sub_pages/tour_map.dart
+++ lib/screens/tour_screen/Single_tour_screen/sub_pages/tour_map.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tour_drive_frontend/screens/tour_screen/single_tour_screen/single_tour_screen.dart';
 import 'package:tour_drive_frontend/widgets/header.dart';
+// import 'package:mapbox_gl/mapbox_gl.dart';
 
 class TourMapScreen extends StatefulWidget {
   const TourMapScreen({super.key});
@@ -10,6 +11,17 @@ class TourMapScreen extends StatefulWidget {
 }
 
 class _TourMapScreenState extends State<TourMapScreen> {
+  // List<LatLng> locations = [
+  // LatLng(37.7749, -122.4194), // San Francisco
+  // LatLng(40.7128, -74.0060), // New York
+  // LatLng(51.5074, -0.1278), // London
+// ];
+@override
+  void initState() {
+    super.initState();
+    //MapboxMapController.registerWith();
+  }
+
   @override
   Widget build(BuildContext context) {
 
@@ -17,25 +29,24 @@ class _TourMapScreenState extends State<TourMapScreen> {
 
     return SafeArea(
       child: Scaffold(
-        body: SingleChildScrollView(
-          scrollDirection: Axis.vertical,
-          child: Column(
-            children: [
-              Header(text: "Tour Map", press: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) => const SingleTourScreen()));
-              } ),
-              Container(
-                margin: EdgeInsets.symmetric(horizontal: screenWidth * 0.05),
-                child: SingleChildScrollView(
-                  scrollDirection: Axis.vertical,
-                  child: Column(
-                    
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
+        // body: MapboxMap(
+        //       accessToken: 'pk.eyJ1IjoiY2hlcmlucGl5dW1hbnRoYSIsImEiOiJja2xuNGprczQwZnBjMm5uM3NwZ2Q1bjY5In0.WmrYfERdPhdhkkuXwcDC8g',
+        //       initialCameraPosition: CameraPosition(
+        //         target: locations[0],
+        //         zoom: 4,
+        //       ),
+        //       onMapCreated: (MapboxMapController controller) {
+        //         // You can customize the map here
+        //         for (var location in locations) {
+        //           controller.addSymbol(SymbolOptions(
+        //             geometry: location,
+        //             iconImage: 'airport-15',
+        //             iconSize: 1.5,
+        //             iconColor: '#FF0000', // This is an example icon
+        //           ));
+        //         }
+        //       },
+        //     ),
       ),
     );
   }
